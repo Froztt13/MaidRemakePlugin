c2d1c604c6745cfe58c23d30d6871c05bc6e15b3
Oni Ahmad <81442964+afif53ptian@users.noreply.github.com>
2021-08-31 10:56:00 +0700

    cell jumper refactor

--- CellJumperHandler.cs
+++ CellJumperHandler.cs
@@ -16,9 +16,7 @@ namespace MaidRemake
 
         public void Handle(XtMessage message)
         {
-
-            /*
-             * Ketika Jalan
+            /* When walk
                 % xt % uotls % -1 % username % sp:8,tx: 486,ty: 427,strFrame: Enter %
 
                 0 =
@@ -27,8 +25,7 @@ namespace MaidRemake
                 3 = -1
                 4 = username
                 5 = sp:8,tx: 486,ty: 427,strFrame: Enter
-
-            * Ketika awal masuk room
+            * when jump into cell
                 % xt % uotls % -1 % username % strPad:Spawn,tx:0,strFrame:Enter,ty:0 %
                 
                 0 =
@@ -37,36 +34,25 @@ namespace MaidRemake
                 3 = -1
                 4 = username
                 5 = strPad:Spawn,tx:0,strFrame:Enter,ty:0
-             */
+            */
 
-            // strPad:Spawn (0), tx:0 (1), strFrame:Enter (2), ty:0 (3)
-            string[] args5_decomp = message.Arguments[5].Split(',');
-
-            // strPad:Spawn (0)
-            string[] args5_strPad = args5_decomp[0].Split(':');
-            string strPad = args5_strPad[0];
-
-            // strFrame:Enter (2)
-            string[] args5_strFrame = args5_decomp[2].Split(':');
-
-            // current Username
-            string currUsername = message.Arguments[4];
-
-            string curr_pad = "Spawn";
-            string curr_cell = "Enter";
-            bool isStrPad = false;
-            if (strPad == "strPad")
+            try
             {
-                isStrPad = true;
-                curr_pad = args5_strPad[1];
-                curr_cell = args5_strFrame[1];
-            }
+                // current Username
+                string currUsername = message.Arguments[4].ToLower();
 
+                if (message.Arguments[5].StartsWith("strPad:") && (currUsername == targetUsername) && !World.IsMapLoading)
+                {
+                    // strPad:Spawn (0), tx:0 (1), strFrame:Enter (2), ty:0 (3)
+                    string targetPad = message.Arguments[5].Split(',')[0].Split(':')[1];
 
-            if (isStrPad && (currUsername == targetUsername) && !World.IsMapLoading)
-            {
-                Player.MoveToCell(curr_cell, curr_pad);
+                    // strPad:Spawn (0), tx:0 (1), strFrame:Enter (2), ty:0 (3)
+                    string targetFrame = message.Arguments[5].Split(',')[2].Split(':')[1];
+
+                    Player.MoveToCell(targetFrame, targetPad);
+                }
             }
+            catch { }
         }
     }
 }
